7b058046ecbdd182612d1aad167a90a0cdd26462
Stas Kobzar <stas@modulis.ca>
2018-01-23 14:30:26 -0500

    Update PJSIP options util with logger.

--- pjsip/options.c
+++ pjsip/options.c
@@ -10,13 +10,60 @@
 #define TARGET    "sip:55555@192.168.1.139:5060"
 #define FROM      "sip:alice@192.168.1.139"
 
-static void endpt_send_cb(pjsip_send_state *st, pj_ssize_t sent, pj_bool_t *cont)
+static pj_bool_t RESPONDED = PJ_FALSE;
+static pj_bool_t logging_on_rx_msg(pjsip_rx_data *rdata);
+static pj_status_t logging_on_tx_msg(pjsip_tx_data *tdata);
+static pjsip_module msg_logger =
 {
-  (void)sent;
-  (void)cont;
-  char buf[1024];
-  pjsip_msg_print(st->tdata->msg, buf, sizeof(buf));
-  PJ_LOG(3, (NAME, "Callback sent message:\n\n%s", buf));
+  NULL, NULL,                 /* prev, next.    */
+  { "mod-msg-log", 13 },      /* Name.    */
+  -1,                         /* Id      */
+  PJSIP_MOD_PRIORITY_TRANSPORT_LAYER-1, /* Priority          */
+  NULL,                       /* load()    */
+  NULL,                       /* start()    */
+  NULL,                       /* stop()    */
+  NULL,                       /* unload()    */
+  &logging_on_rx_msg,         /* on_rx_request()  */
+  &logging_on_rx_msg,         /* on_rx_response()  */
+  &logging_on_tx_msg,         /* on_tx_request.  */
+  &logging_on_tx_msg,         /* on_tx_response()  */
+  NULL,                       /* on_tsx_state()  */
+
+};
+
+/* Notification on incoming messages */
+static pj_bool_t logging_on_rx_msg(pjsip_rx_data *rdata)
+{
+  PJ_LOG(3,(NAME, "RX %d bytes %s from %s %s:%d:\n"
+        "%.*s\n"
+        "--end msg--",
+        rdata->msg_info.len,
+        pjsip_rx_data_get_info(rdata),
+        rdata->tp_info.transport->type_name,
+        rdata->pkt_info.src_name,
+        rdata->pkt_info.src_port,
+        (int)rdata->msg_info.len,
+        rdata->msg_info.msg_buf));
+
+  RESPONDED = PJ_TRUE;
+
+  return PJ_FALSE;
+}
+
+/* Notification on outgoing messages */
+static pj_status_t logging_on_tx_msg(pjsip_tx_data *tdata)
+{
+  PJ_LOG(3,(NAME, "TX %d bytes %s to %s %s:%d:\n"
+        "%.*s\n"
+        "--end msg--",
+        (tdata->buf.cur - tdata->buf.start),
+        pjsip_tx_data_get_info(tdata),
+        tdata->tp_info.transport->type_name,
+        tdata->tp_info.dst_name,
+        tdata->tp_info.dst_port,
+        (int)(tdata->buf.cur - tdata->buf.start),
+        tdata->buf.start));
+  return PJ_SUCCESS;
 }
 
 int main(int argc, const char *argv[])
@@ -41,6 +88,10 @@ int main(int argc, const char *argv[])
     PJ_LOG(3, (NAME, "ERROR: Failed to create end-point."));
     return 1;
   }
+
+  status = pjsip_endpt_register_module(endpt, &msg_logger);
+  PJ_ASSERT_RETURN(status == PJ_SUCCESS, status);
+
   pj_sockaddr_in_init(&addr, NULL, 15060);
 
   PJ_LOG(3, (NAME, "======= OPTIONS example ======"));
@@ -72,12 +123,17 @@ int main(int argc, const char *argv[])
   }
 
   // this function will call pjsip_transport_send()
-  status = pjsip_endpt_send_request_stateless(endpt, tdata, NULL, endpt_send_cb);
+  status = pjsip_endpt_send_request_stateless(endpt, tdata, NULL, NULL);
   if (status != PJ_SUCCESS) {
     PJ_LOG(3, (NAME, "ERROR: Failed to create request."));
     return 1;
   }
 
+  // main loop. Exit on response.
+  for (;;) {
+    pjsip_endpt_handle_events(endpt, NULL);
+    if (RESPONDED == PJ_TRUE) break;
+  }
   // done
   pj_caching_pool_destroy(&cpool);
 
